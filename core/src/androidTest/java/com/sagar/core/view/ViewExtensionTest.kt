52630a9089aa44fcd72094868c0e62421910d61d
Sagar Viradiya <sagarviradiya4444@gmail.com>
2021-03-28 20:46:14 +0530

    Test enhancements
    - Remove all dependencies from test module to avoid multidex apk.
    - Replace ActivityScenarioRule with ActivityTestRule for backward compatibility.
    - Update gradle plugin.

--- core/src/androidTest/java/com/sagar/core/view/ViewExtensionTest.kt
+++ core/src/androidTest/java/com/sagar/core/view/ViewExtensionTest.kt
@@ -1,39 +1,26 @@
 package com.sagar.core.view
 
-import androidx.lifecycle.Lifecycle
-import androidx.test.ext.junit.rules.activityScenarioRule
+import androidx.test.rule.ActivityTestRule
 import com.sagar.test.TestActivity
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
 class ViewExtensionTest {
 
+    // TODO : Replace this with ActivityScenarioRule
     @get:Rule
-    val activityRule = activityScenarioRule<TestActivity>()
-
-    private lateinit var activity: TestActivity
-
-    @Before
-    fun before() {
-        activityRule.scenario.moveToState(Lifecycle.State.RESUMED)
-        runBlocking(Dispatchers.Main.immediate) {
-            activityRule.scenario.onActivity {
-                activity = it
-            }
-        }
-    }
+    val activityRule = ActivityTestRule(TestActivity::class.java)
 
     @Test
     fun postTest() = runBlocking(Dispatchers.Default) {
-        Assert.assertTrue(activity.demoView.awaitPost())
+        Assert.assertTrue(activityRule.activity.demoView.awaitPost())
     }
 
     @Test
     fun postDelayTest() = runBlocking(Dispatchers.Default) {
-        Assert.assertTrue(activity.demoView.awaitPostDelay(2000))
+        Assert.assertTrue(activityRule.activity.demoView.awaitPostDelay(2000))
     }
 }
