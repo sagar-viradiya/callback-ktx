79c72d8a1dd2d1f44d362fb4ceba5b7251ba6baa
Sagar Viradiya <sagarviradiya4444@gmail.com>
2021-03-28 20:46:14 +0530

    Test enhancements
    - Remove all dependencies from test module to avoid multidex apk.
    - Replace ActivityScenarioRule with ActivityTestRule for backward compatibility.
    - Update gradle plugin.

--- test/src/main/java/com/sagar/test/TestActivity.kt
+++ test/src/main/java/com/sagar/test/TestActivity.kt
@@ -1,8 +1,14 @@
 package com.sagar.test
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.os.Bundle
 import android.view.View
 
-class TestActivity : AppCompatActivity(R.layout.activity_test) {
+class TestActivity : Activity() {
     val demoView: View by lazy { findViewById(R.id.demo_view) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test)
+    }
 }
