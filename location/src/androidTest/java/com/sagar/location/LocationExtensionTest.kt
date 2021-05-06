2d11c1ee5140181d816bae1115c000028922c480
Sagar Viradiya <sagarviradiya4444@gmail.com>
2021-05-07 00:04:58 +0530

    CI changes and location test changes
    - Separate emulators with playstore support(Google play services)
    - Skip location test on API levels below 24

--- location/src/androidTest/java/com/sagar/location/LocationExtensionTest.kt
+++ location/src/androidTest/java/com/sagar/location/LocationExtensionTest.kt
@@ -1,6 +1,7 @@
 package com.sagar.location
 
 import android.Manifest
+import android.os.Build.VERSION.SDK_INT
 import androidx.lifecycle.Lifecycle
 import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.rule.GrantPermissionRule
@@ -17,6 +18,7 @@ import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.take
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert
+import org.junit.Assume.assumeTrue
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -41,90 +43,110 @@ class LocationExtensionTest {
     }
 
     @Test
-    fun lastLocationTest() = runBlocking {
-        try {
-            val location = fusedLocationProviderClient.awaitLastLocation()
-            location?.let {
-                Assert.assertTrue(!it.isFromMockProvider)
-                Assert.assertTrue(it.latitude != 0.0)
-                Assert.assertTrue(it.longitude != 0.0)
-            } ?: Assert.assertTrue(true)
-        } catch (exception: Exception) {
-            // If there is any exception while getting last location we need to still pass this test
-            Assert.assertTrue(true)
+    fun lastLocationTest() {
+        /*Emulator with google play service is only available form API level 24 and above
+        so skip test for all API below 24*/
+        assumeTrue(SDK_INT > 23)
+        runBlocking {
+            try {
+                val location = fusedLocationProviderClient.awaitLastLocation()
+                location?.let {
+                    Assert.assertTrue(!it.isFromMockProvider)
+                    Assert.assertTrue(it.latitude != 0.0)
+                    Assert.assertTrue(it.longitude != 0.0)
+                } ?: Assert.assertTrue(true)
+            } catch (exception: Exception) {
+                // If there is any exception while getting last location we need to still pass this test
+                Assert.assertTrue(true)
+            }
         }
     }
 
     @Test
-    fun locationFlowTest() = runBlocking(Dispatchers.Main.immediate) {
-        val locationFlow = fusedLocationProviderClient.locationFlow(
-            LocationRequest.create().apply {
-                interval = 100
-                fastestInterval = 100
-                priority = LocationRequest.PRIORITY_LOW_POWER
-            },
-            testActivity
-        )
-
-        locationFlow.take(2).collect {
-            Assert.assertTrue(!it.isFromMockProvider)
-            Assert.assertTrue(it.latitude != 0.0)
-            Assert.assertTrue(it.longitude != 0.0)
-        }
-    }
-
-    @Test
-    fun activityResumePauseDestroyTest() = runBlocking(Dispatchers.Default) {
-        activityScenarioRule.scenario.moveToState(Lifecycle.State.CREATED)
-        var shouldEmit = false
-        var flowCompleted = false
-        val deferredAssertions = async(Dispatchers.Main.immediate) {
-            fusedLocationProviderClient.locationFlow(
+    fun locationFlowTest() {
+        /*Emulator with google play service is only available form API level 24 and above
+        so skip test for all API below 24*/
+        assumeTrue(SDK_INT > 23)
+        runBlocking(Dispatchers.Main.immediate) {
+            val locationFlow = fusedLocationProviderClient.locationFlow(
                 LocationRequest.create().apply {
                     interval = 100
                     fastestInterval = 100
                     priority = LocationRequest.PRIORITY_LOW_POWER
                 },
                 testActivity
-            ).onCompletion {
-                flowCompleted = true
-            }.collect {
-                Assert.assertTrue(shouldEmit)
-            }
-        }
-        shouldEmit = true
-        activityScenarioRule.scenario.moveToState(Lifecycle.State.RESUMED)
-        delay(500)
-        activityScenarioRule.scenario.moveToState(Lifecycle.State.CREATED)
-        delay(500)
-        shouldEmit = false
-        delay(500)
-        activityScenarioRule.scenario.moveToState(Lifecycle.State.DESTROYED)
-        deferredAssertions.await()
-        Assert.assertTrue(flowCompleted)
-    }
+            )
 
-    @Test
-    fun locationFlowCancelTest() = runBlocking {
-        var flowCompleted = false
-        val deferredAssertions = async(Dispatchers.Main.immediate) {
-            fusedLocationProviderClient.locationFlow(
-                LocationRequest.create().apply {
-                    interval = 100
-                    fastestInterval = 100
-                    priority = LocationRequest.PRIORITY_LOW_POWER
-                },
-                testActivity
-            ).onCompletion {
-                flowCompleted = true
-            }.collect {
+            locationFlow.take(2).collect {
                 Assert.assertTrue(!it.isFromMockProvider)
                 Assert.assertTrue(it.latitude != 0.0)
                 Assert.assertTrue(it.longitude != 0.0)
             }
         }
-        delay(2000)
-        deferredAssertions.cancelAndJoin()
-        Assert.assertTrue(flowCompleted)
+    }
+
+    @Test
+    fun activityResumePauseDestroyTest() {
+        /*Emulator with google play service is only available form API level 24 and above
+        so skip test for all API below 24*/
+        assumeTrue(SDK_INT > 23)
+        runBlocking(Dispatchers.Default) {
+            activityScenarioRule.scenario.moveToState(Lifecycle.State.CREATED)
+            var shouldEmit = false
+            var flowCompleted = false
+            val deferredAssertions = async(Dispatchers.Main.immediate) {
+                fusedLocationProviderClient.locationFlow(
+                    LocationRequest.create().apply {
+                        interval = 100
+                        fastestInterval = 100
+                        priority = LocationRequest.PRIORITY_LOW_POWER
+                    },
+                    testActivity
+                ).onCompletion {
+                    flowCompleted = true
+                }.collect {
+                    Assert.assertTrue(shouldEmit)
+                }
+            }
+            shouldEmit = true
+            activityScenarioRule.scenario.moveToState(Lifecycle.State.RESUMED)
+            delay(500)
+            activityScenarioRule.scenario.moveToState(Lifecycle.State.CREATED)
+            delay(500)
+            shouldEmit = false
+            delay(500)
+            activityScenarioRule.scenario.moveToState(Lifecycle.State.DESTROYED)
+            deferredAssertions.await()
+            Assert.assertTrue(flowCompleted)
+        }
+    }
+
+    @Test
+    fun locationFlowCancelTest() {
+        /*Emulator with google play service is only available form API level 24 and above
+        so skip test for all API below 24*/
+        assumeTrue(SDK_INT > 23)
+        runBlocking {
+            var flowCompleted = false
+            val deferredAssertions = async(Dispatchers.Main.immediate) {
+                fusedLocationProviderClient.locationFlow(
+                    LocationRequest.create().apply {
+                        interval = 100
+                        fastestInterval = 100
+                        priority = LocationRequest.PRIORITY_LOW_POWER
+                    },
+                    testActivity
+                ).onCompletion {
+                    flowCompleted = true
+                }.collect {
+                    Assert.assertTrue(!it.isFromMockProvider)
+                    Assert.assertTrue(it.latitude != 0.0)
+                    Assert.assertTrue(it.longitude != 0.0)
+                }
+            }
+            delay(2000)
+            deferredAssertions.cancelAndJoin()
+            Assert.assertTrue(flowCompleted)
+        }
     }
 }
