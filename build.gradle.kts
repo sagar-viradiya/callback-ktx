83452519ea8d2d5f5548322cb57d36eeda867434
Sagar Viradiya <sagarviradiya4444@gmail.com>
2021-07-04 22:08:39 +0530

    Maven publish setup
    - Add gradle maven publish plugin
    - Add POM details in the gradle.properties
    - Add module specific POM details in the module's gradle.properties
    - Add job in github action workflow to upload snapshots
    - Add publish script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,6 @@
 import kotlinx.validation.ApiValidationExtension
+import com.vanniktech.maven.publish.MavenPublishPluginExtension
+import com.vanniktech.maven.publish.SonatypeHost
 
 plugins {
     id("org.jetbrains.kotlinx.binary-compatibility-validator") version Versions.KOTLIN_BINARY_COMPATIBILITY
@@ -14,6 +16,7 @@ buildscript {
         classpath("com.android.tools.build:gradle:${Versions.GRADLE_PLUGIN}")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.KOTLIN}")
         classpath("org.jlleitschuh.gradle:ktlint-gradle:${Versions.KTLINT_PLUGIN}")
+        classpath("com.vanniktech:gradle-maven-publish-plugin:${Versions.MAVEN_PUBLISH_PLUGIN}")
     }
 }
 
@@ -21,6 +24,10 @@ allprojects {
     repositories {
         google()
         mavenCentral()
+    }
+
+    extensions.getByType<MavenPublishPluginExtension>().apply {
+        sonatypeHost = SonatypeHost.S01
     }
 }
 
